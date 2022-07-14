b2d2efb96644705e4f2e89a369c8e47051f7e975
nin-jin <nin-jin@users.noreply.github.com>
2022-07-14 07:14:00 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -6299,6 +6299,9 @@ var $;
         enabled() {
             return true;
         }
+        bring() {
+            return this.Query().bring();
+        }
         Query() {
             const obj = new this.$.$mol_string();
             obj.value = (val) => this.query(val);
