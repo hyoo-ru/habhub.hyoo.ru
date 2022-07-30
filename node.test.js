340bd3fece69289eec1b3dda579d0fcfe350f5e1
nin-jin <nin-jin@users.noreply.github.com>
2022-07-30 07:13:15 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -4630,6 +4630,7 @@ var $;
             return {
                 ...super.event(),
                 click: (event) => this.event_activate(event),
+                dblclick: (event) => this.clicks(event),
                 keydown: (event) => this.event_key_press(event)
             };
         }
@@ -4653,6 +4654,11 @@ var $;
             return obj;
         }
         event_activate(event) {
+            if (event !== undefined)
+                return event;
+            return null;
+        }
+        clicks(event) {
             if (event !== undefined)
                 return event;
             return null;
@@ -4687,6 +4693,9 @@ var $;
     __decorate([
         $mol_mem
     ], $mol_button.prototype, "event_activate", null);
+    __decorate([
+        $mol_mem
+    ], $mol_button.prototype, "clicks", null);
     __decorate([
         $mol_mem
     ], $mol_button.prototype, "event_key_press", null);
--- web.d.ts
+++ web.d.ts
@@ -1447,6 +1447,7 @@ declare namespace $ {
         event_click(event?: any): any;
         event(): {
             click: (event?: any) => any;
+            dblclick: (event?: any) => any;
             keydown: (event?: any) => any;
         };
         attr(): {
@@ -1458,6 +1459,7 @@ declare namespace $ {
         sub(): readonly $mol_view_content[];
         Speck(): $mol_speck;
         event_activate(event?: any): any;
+        clicks(event?: any): any;
         event_key_press(event?: any): any;
         disabled(): boolean;
         tab_index(): number;
