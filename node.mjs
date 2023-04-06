bc7acaed54743562f813c3ce645f4b8a609e23d4
nin-jin <nin-jin@users.noreply.github.com>
2023-04-06 17:34:54 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.mjs
+++ node.mjs
@@ -5485,8 +5485,8 @@ var $;
             return {
                 ...super.attr(),
                 mol_check_checked: this.checked(),
-                "aria-checked": this.checked(),
-                role: "checkbox"
+                "aria-checked": this.aria_checked(),
+                role: this.aria_role()
             };
         }
         sub() {
@@ -5495,10 +5495,16 @@ var $;
                 this.label()
             ];
         }
-        checked(val) {
-            if (val !== undefined)
-                return val;
+        checked(next) {
+            if (next !== undefined)
+                return next;
             return false;
+        }
+        aria_checked() {
+            return "false";
+        }
+        aria_role() {
+            return "checkbox";
         }
         Icon() {
             return null;
@@ -5567,6 +5573,9 @@ var $;
             }
             label() {
                 return this.title() ? super.label() : [];
+            }
+            aria_checked() {
+                return String(this.checked());
             }
         }
         $$.$mol_check = $mol_check;
--- web.mjs
+++ web.mjs
@@ -4872,8 +4872,8 @@ var $;
             return {
                 ...super.attr(),
                 mol_check_checked: this.checked(),
-                "aria-checked": this.checked(),
-                role: "checkbox"
+                "aria-checked": this.aria_checked(),
+                role: this.aria_role()
             };
         }
         sub() {
@@ -4882,10 +4882,16 @@ var $;
                 this.label()
             ];
         }
-        checked(val) {
-            if (val !== undefined)
-                return val;
+        checked(next) {
+            if (next !== undefined)
+                return next;
             return false;
+        }
+        aria_checked() {
+            return "false";
+        }
+        aria_role() {
+            return "checkbox";
         }
         Icon() {
             return null;
@@ -4944,6 +4950,9 @@ var $;
             }
             label() {
                 return this.title() ? super.label() : [];
+            }
+            aria_checked() {
+                return String(this.checked());
             }
         }
         $$.$mol_check = $mol_check;
