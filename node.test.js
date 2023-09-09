7fad8c133c23914889b7ccd8e7443cfc18826ca3
nin-jin <nin-jin@users.noreply.github.com>
2023-09-09 07:05:27 +0000

    Deploying to gh-pages from @ hyoo-ru/habhub.hyoo.ru@3a78c2295cf5c579ecb6fdf879e011103bfe3a3e 🚀

--- node.test.js
+++ node.test.js
@@ -8726,6 +8726,11 @@ var $;
             Rows: {
                 padding: $mol_gap.text,
             },
+            Row: {
+                font: {
+                    family: 'inherit',
+                },
+            },
             Copy: {
                 alignSelf: 'flex-start',
                 justifySelf: 'flex-start',
