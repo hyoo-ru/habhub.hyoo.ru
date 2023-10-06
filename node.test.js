d31ab7af459befe14af4f999594aac8ea4e6bcbc
nin-jin <nin-jin@users.noreply.github.com>
2023-10-06 07:07:39 +0000

    Deploying to gh-pages from @ hyoo-ru/habhub.hyoo.ru@3a78c2295cf5c579ecb6fdf879e011103bfe3a3e 🚀

--- node.test.js
+++ node.test.js
@@ -6209,6 +6209,9 @@ var $;
                 ]);
                 el.selectionEnd = to;
                 el.selectionStart = from;
+                if (to !== from && el.selectionEnd === el.selectionStart) {
+                    el.selectionEnd = to;
+                }
             }
             selection_start() {
                 const el = this.dom_node();
