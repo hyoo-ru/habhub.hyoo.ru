888f73d8af162b8f19fefa7c19b8e8c6ffe0384d
nin-jin <nin-jin@users.noreply.github.com>
2023-10-10 07:07:28 +0000

    Deploying to gh-pages from @ hyoo-ru/habhub.hyoo.ru@3a78c2295cf5c579ecb6fdf879e011103bfe3a3e 🚀

--- node.js
+++ node.js
@@ -3660,6 +3660,15 @@ var $;
         dom_name() {
             return "a";
         }
+        uri_off() {
+            return "";
+        }
+        uri_native() {
+            return null;
+        }
+        external() {
+            return false;
+        }
         attr() {
             return {
                 ...super.attr(),
--- web.mjs
+++ web.mjs
@@ -3100,6 +3100,15 @@ var $;
         dom_name() {
             return "a";
         }
+        uri_off() {
+            return "";
+        }
+        uri_native() {
+            return null;
+        }
+        external() {
+            return false;
+        }
         attr() {
             return {
                 ...super.attr(),
