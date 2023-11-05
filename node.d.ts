7ff45f9def0fad6d19db70686a6cfdd5b9f86a89
nin-jin <nin-jin@users.noreply.github.com>
2023-11-05 07:05:43 +0000

    Deploying to gh-pages from @ hyoo-ru/habhub.hyoo.ru@3a78c2295cf5c579ecb6fdf879e011103bfe3a3e 🚀

--- node.d.ts
+++ node.d.ts
@@ -1851,6 +1851,7 @@ declare namespace $ {
         event(): Record<string, any>;
         plugins(): readonly any[];
         selection_watcher(): any;
+        error_report(): any;
         disabled(): boolean;
         value(next?: any): string;
         value_changed(next?: any): string;
@@ -1874,6 +1875,7 @@ declare namespace $ {
 declare namespace $.$$ {
     class $mol_string extends $.$mol_string {
         event_change(next?: Event): void;
+        error_report(): void;
         hint_visible(): string;
         disabled(): boolean;
         autocomplete_native(): "on" | "off";
