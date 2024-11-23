edcf37d070aa160fc33055c91d4e999ce8f74b0b
nin-jin <nin-jin@users.noreply.github.com>
2024-11-23 07:09:26 +0000

    Deploying to gh-pages from @ hyoo-ru/habhub.hyoo.ru@1ce0328a6b44c1a317b8a0da6fe9fb0c32f53fe9 🚀

--- node.test.js
+++ node.test.js
@@ -7488,8 +7488,8 @@ var $;
 		dom_name(){
 			return "article";
 		}
-		field(){
-			return {...(super.field()), "tabIndex": (this.tabindex())};
+		attr(){
+			return {...(super.attr()), "tabIndex": (this.tabindex())};
 		}
 		sub(){
 			return [
