28d78bfc39926f8d9250364fe7db711898320ec0
nin-jin <nin-jin@users.noreply.github.com>
2025-01-02 07:09:32 +0000

    Deploying to gh-pages from @ hyoo-ru/habhub.hyoo.ru@1ce0328a6b44c1a317b8a0da6fe9fb0c32f53fe9 🚀

--- node.d.ts
+++ node.d.ts
@@ -3549,7 +3549,7 @@ declare namespace $ {
 		uri( ): string
 		loading( ): string
 		decoding( ): string
-		cors( ): any
+		cors( ): string
 		natural_width( ): number
 		natural_height( ): number
 		load( next?: any ): any
