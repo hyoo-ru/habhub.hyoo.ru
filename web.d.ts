8008a48074bb51e1381336146b177e54231b6d19
nin-jin <nin-jin@users.noreply.github.com>
2025-02-11 07:09:21 +0000

    Deploying to gh-pages from @ hyoo-ru/habhub.hyoo.ru@1ce0328a6b44c1a317b8a0da6fe9fb0c32f53fe9 🚀

--- web.d.ts
+++ web.d.ts
@@ -496,6 +496,20 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    class $mol_promise<Result = void> extends Promise<Result> {
+        done: (value: Result | PromiseLike<Result>) => void;
+        fail: (reason?: any) => void;
+        constructor(executor?: (done: (value: Result | PromiseLike<Result>) => void, fail: (reason?: any) => void) => void);
+    }
+}
+
+declare namespace $ {
+    class $mol_promise_blocker<Result> extends $mol_promise<Result> {
+        static [Symbol.toStringTag]: string;
+    }
+}
+
+declare namespace $ {
     class $mol_decor<Value> {
         readonly value: Value;
         constructor(value: Value);
@@ -2552,13 +2566,6 @@ declare namespace $ {
 }
 
 //# sourceMappingURL=new.view.tree.d.ts.map
-declare namespace $ {
-    function $mol_promise<Result = void>(): Promise<Result> & {
-        done: (res: Result | PromiseLike<Result>) => void;
-        fail: (error?: any) => void;
-    };
-}
-
 declare namespace $ {
     function $mol_wait_timeout_async(this: $, timeout: number): Promise<void>;
     function $mol_wait_timeout(this: $, timeout: number): void;
